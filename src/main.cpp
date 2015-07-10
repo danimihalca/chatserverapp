54607f0163d55bbdd4465a9dab7786604745bfa1
Daniel Mihalca <danimihalca@gmail.com>
2015-07-10 12:04:51 +0300

    Broadcast received messages to all connected clients

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,45 @@
 
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
+#include <set>
 
-typedef websocketpp::server<websocketpp::config::asio> server;
-server print_server;
+typedef websocketpp::server<websocketpp::config::asio> broadcastServer;
+broadcastServer print_server;
+std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> connections;
 
-void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg)
+void on_message(websocketpp::connection_hdl hdl, broadcastServer::message_ptr msg)
 {
     std::cout <<"MESSAGE RECEIVED: " + msg->get_payload() << std::endl;
-
-    server::connection_ptr con = print_server.get_con_from_hdl(hdl);
-    con->send(std::string{"This is the reply for: \"" +  msg->get_payload() +'\"'});
+    for(auto connection: connections)
+    {
+        print_server.send(connection,msg);
+    }
 }
 
 void on_close(websocketpp::connection_hdl hdl)
 {
-    server::connection_ptr con = print_server.get_con_from_hdl(hdl);
+    connections.erase(hdl);
+    broadcastServer::connection_ptr con = print_server.get_con_from_hdl(hdl);
     std::cout <<"Disconnected: " + con->get_remote_endpoint() <<std::endl;
 }
 
 void on_open(websocketpp::connection_hdl hdl)
 {
-    server::connection_ptr con = print_server.get_con_from_hdl(hdl);
+    connections.insert(hdl);
+    broadcastServer::connection_ptr con = print_server.get_con_from_hdl(hdl);
     std::cout <<"Connected: " + con->get_remote_endpoint() <<std::endl;
+    std::cout <<"Origin:" + con->get_host()<<std::endl;
 }
+
 
 int main()
 {
-
     print_server.set_message_handler(&on_message);
     print_server.set_close_handler(&on_close);
     print_server.set_open_handler(&on_open);
     print_server.init_asio();
-    print_server.clear_access_channels(websocketpp::log::alevel::all);
-//    print_server.set_access_channels(websocketpp::log::alevel::connect);
-//    print_server.set_access_channels(websocketpp::log::alevel::disconnect);
-//    print_server.set_access_channels(websocketpp::log::alevel::app);
+    print_server.set_access_channels(websocketpp::log::alevel::all);
+    print_server.set_error_channels(websocketpp::log::elevel::all);
 
     print_server.listen(9002);
     print_server.start_accept();
